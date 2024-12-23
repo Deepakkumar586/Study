6bc6a9258004fc2bbdf503c8f2dc0387967f5629
Deepak <aryanrajj1234@gmail.com>
2024-12-23 15:36:18 +0530

    fix the cors issue

--- Server/index.js
+++ Server/index.js
@@ -35,8 +35,12 @@ const allowedOrigins = [
 
 // Debugging middleware to log origins
 app.use((req, res, next) => {
-  const origin = req.headers.origin || "undefined";
-  console.log(`[server] Request Origin: ${origin}`);
+  res.setHeader("Access-Control-Allow-Origin", "*"); // You can replace '*' with specific origin
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
